cae2614bfa7d1863b943a871912d7942a81b3331
Nisarg Chaudhari <nisrg5601@gmail.com>
2022-02-24 16:40:36 +0530

    register and getAddress functions added

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+    // "mapping" to store names
+    mapping(string => address) public domains;
+
     constructor() {
         console.log("Console log from the domain contract!!");
+    }
+
+    // Register function to add names to our mapping
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has register a domain %s.gmi!!",msg.sender,name);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,18 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
-    console.log("Contract deployed to:", domainContract.address);
+
+    console.log("Contract deployed to: ", domainContract.address);
+    console.log("Contract deployed by: ", owner.address)
+
+    const txn = await domainContract.register("wagmi");
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress("wagmi");
+    console.log("Owner of domain:", domainOwner);
+
 };
 
 const runMain = async() => {
