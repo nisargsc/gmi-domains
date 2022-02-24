c5c911292250b9f6087f3569911d68b0828329dc
Nisarg Chaudhari <nisarg5601@gmail.com>
2022-02-24 17:44:15 +0530

    Domain contract with resolver and records (#2)
    * register and getAddress functions added
    * get and set records

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,35 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
-    console.log("Contract deployed to:", domainContract.address);
+
+    console.log("Contract deployed to: ", domainContract.address);
+    console.log("Contract deployed by: ", owner.address)
+
+    let txn = await domainContract.register("wagmi");
+    await txn.wait();
+
+    let domainOwner = await domainContract.getAddress("wagmi");
+    console.log("Owner of domain:", domainOwner);
+
+    // randomPerson trying to change the record that doesn't belong to him.... How dare he do that??
+    txn = await domainContract.setRecord("wagmi", "Hey, We are all gonna make it!!");
+    await txn.wait();
+
+    let domainRecord = await domainContract.getRecord("wagmi");
+    console.log("Record for wagami.gmi:", domainRecord)
+
+    txn = await domainContract.connect(randomPerson).register("randomPerson");
+    await txn.wait();
+
+    domainOwner = await domainContract.getAddress("randomPerson");
+    console.log("Owner of domain:", domainOwner);
+
+    txn = await domainContract.connect(randomPerson).setRecord("randomPerson", "Even me the randomPerson is gonna make it")
+    
+    domainRecord = await domainContract.getRecord("randomPerson");
+    console.log("Record for randomPerson.gmi:", domainRecord)
 };
 
 const runMain = async() => {
